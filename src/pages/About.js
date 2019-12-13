772a077259d0c6bb87b9dab5c5e7b1d5ae8d8e7a
Kyle C <kyclutter@gmail.com>
2019-12-13 08:41:25 -0600

    first

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom';
 
 function About() {
   return (
-    <div className="About">
-      <h1>About page</h1>
-      <Link to="/">Go to Home page</Link>
+    <div className="about">
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import today from '../assets/today.png';
+import cbs from '../assets/cbs.png';
 
-function Home() {
-  return (
-    <div className="Home">
-      <h1>Home page</h1>
-      <Link to="/about">Go to About page</Link>
-    </div>
-  )
+
+class Home extends Component {
+  
+
+  render(){
+
+    let items =  [[today, cbs, today], [today, cbs, today], [today, cbs, today]].map((cur, i) =>(
+      <div className="image">
+        <img className="image-1" src={cur[0]} alt=""/>
+        <img className="image-2" src={cur[1]} alt=""/>
+        <img src={cur[2]} alt=""/>
+      </div>
+  
+    ))
+
+    
+    
+    
+   
+
+    return (
+      <div className="Home">
+        <h1>Home page</h1>
+
+
+
+        <div className="Home__media">
+          <div className="Home__media-header"></div>
+          <div className="Home__media-header-text">Amy In The Media</div>
+          <div className="Home__media-images">
+            <AliceCarousel 
+            items={items}
+            duration={1600}
+            autoPlay={false}
+            
+            />
+            
+          </div>
+          
+        </div>
+        
+      </div>
+    )
+  }
+  
 }
 
 export default Home;
