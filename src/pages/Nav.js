b9c4a2a92e8d04df20a6bb803d4aa3e64b721169
Kyle C <kyclutter@gmail.com>
2019-12-17 15:46:35 -0600

    updates

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 
 class Nav extends Component {
   state = {
-    clicked: false
-  };
+    clicked: false,
+    listClick: false
+  }
 
   menu = () => {
     this.setState({
@@ -15,13 +16,21 @@ class Nav extends Component {
     const { clicked } = this.state;
 
     return (
-      <div className={!clicked ? "nav" : "nav-big"} onClick={this.menu}>
-        <div className={!clicked ? "nav-box" : "nav-box-1"}>
+      <div className={!clicked ? "nav" : "nav-big"} >
+        <div className={!clicked ? "nav-box" : "nav-box-1"} onClick={this.menu}>
           <div className={!clicked ? "nav-1" : "nav-1 nav-6"}></div>
           <div className={!clicked ? "nav-2" : " nav-2 nav-5"}></div>
           <div className={!clicked ? "nav-3" : " nav-3 nav-4"}></div>
           <div className={clicked ? "none" : "nav-7"}>Menu</div>
           <div className={clicked ? "nav-8" : "none"}>Exit</div>
+        </div>
+        <div className={clicked ? "nav__menu" : "none"}>
+            <ul>
+                <li className="nav__menu-item">Home</li>
+                <li className="nav__menu-item" >About Amy</li>
+                <li className="nav__menu-item" >Photos</li>
+                <li className="nav__menu-item" >Contact Amy</li>
+            </ul>
         </div>
       </div>
     );
